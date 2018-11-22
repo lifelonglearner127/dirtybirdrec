dc2c9f797497d4877a7364d927a64370149f0e97
lifelonglearner127 <life.long.learner127@outlook.com>
2018-11-22 23:19:19 +0700

    User icons

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -72,7 +72,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     set_flash_message :notice, :destroyed
     yield resource if block_given?
-    redirect_to after_sign_out_path_for(resource_name)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+    redirect_to after_sign_out_path_for(resource_name)
   end  
 
   protected
